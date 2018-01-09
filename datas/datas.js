3a0a64369409013dfb31033bb53d72d2a9e3d518
kinna5 <772345373@qq.com>
2018-01-09 18:18:06 +0800

    跳转

--- datas/datas.js
+++ datas/datas.js
@@ -1,21 +1,29 @@
 const listData = [
   {
+    id:0,
     icon:'/imgs/icon1.jpg',
     time:'2018-01-08 07:31:00',
     title:'雪雕现身',
     img:'/imgs/new1.jpg',
     summary:'美丽的雪雕为城市开启了别样的早晨',
     watch:'0',
-    attach:'123'
+    attach:'123',
+    detail:'详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情',
+    author:'kinna',
+    music:''
   },
   {
+    id:1,
     icon: '/imgs/icon2.jpg',
     time: '2018-01-08 07:00:00',
     title: '栩栩如生的雪雕',
     img: '/imgs/new1.jpg',
     summary: '天气变冷，雪雕宝宝奏出现啦！',
     watch: '0',
-    attach: '123'
+    attach: '123',
+    detail: '详情详情详情详情详情详2222222222情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情',
+    author: 'kinna',
+    music: ''
   }
 ];
 module.exports = {
